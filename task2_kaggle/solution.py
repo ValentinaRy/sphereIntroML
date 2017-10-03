dfc163c01f9ecd2d0d18b1da78c6298c26c2312b
ZhenyaMsl <valeera47@gmail.com>
2017-10-03 14:25:44 +0300

    Add to dictionary keys with length more or equal to 2

--- task2_kaggle/solution.py
+++ task2_kaggle/solution.py
@@ -14,18 +14,20 @@ fl.readline()
 for line in fl:
     Id, Sample, Prediction = line.strip().split(',')
     word1, word2 = Prediction.split(' ')
-    key = word1[:4]
-    if key not in word_stat_dict:
-        word_stat_dict[key] = {}
-    if word1 not in word_stat_dict[key]:
-        word_stat_dict[key][word1] = 0
-    word_stat_dict[key][word1] += 1
-    key = word2[:4]
-    if key not in word_stat_dict:
-        word_stat_dict[key] = {}
-    if word2 not in word_stat_dict[key]:
-        word_stat_dict[key][word2] = 0
-    word_stat_dict[key][word2] += 1
+    for i in range(4, len(word1)+1, 2):
+        key = word1[:i]
+        if key not in word_stat_dict:
+            word_stat_dict[key] = {}
+        if word1 not in word_stat_dict[key]:
+            word_stat_dict[key][word1] = 0
+        word_stat_dict[key][word1] += 1
+    for i in range(4, len(word2)+1, 2):
+        key = word2[:i]
+        if key not in word_stat_dict:
+            word_stat_dict[key] = {}
+        if word2 not in word_stat_dict[key]:
+            word_stat_dict[key][word2] = 0
+        word_stat_dict[key][word2] += 1
 
 fl.close()
 
@@ -44,12 +46,16 @@ out_fl = open(PATH_PRED, 'wt')
 out_fl.write('Id,Prediction\n')
 
 for line in fl:
+    flag = True
     Id, Sample = line.strip().split(',')
     word1, word2_chunk = Sample.split(' ')
-    key = word2_chunk[:4]
-    if key in most_freq_dict:
-        out_fl.write('%s,%s %s\n' % (Id, word1, most_freq_dict[key]) )
-    else:
+    for i in range(len(word2_chunk), 3, -2):
+        key = word2_chunk[:i]
+        if key in most_freq_dict:
+            flag = False
+            out_fl.write('%s,%s %s\n' % (Id, word1, most_freq_dict[key]) )
+            break
+    if (flag):
         out_fl.write('%s,%s %s\n' % (Id, word1, word2_chunk) )
 
 fl.close()
